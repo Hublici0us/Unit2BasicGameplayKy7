0ab780df1c78534732ebcd3d1efe48435279969e
Jason Ky <744169@student.dusd.net>
2024-03-12 15:29:28 -0700

    yuh
    i made bro move and the steak fly as well as making the animals do their thing
    
    INSTANTIATE - (gameObject spawned, position spawned, rotation spawned)

---
+++ BasicGameplayUnit2KyP7/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float horizontalInput;
+    public float speed = 20.0f;
+    public float xRange = 12.0f;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //limits the range of player movement
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        { 
+            transform.position = new Vector3(xRange,transform.position.y, transform.position.z);
+        }
+
+        //controls and tests spacebar projectile
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Pressed.");
+            Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
